393eb0661ce8198ca6c049fdd1b2d3c95b302545
Jungtaek Kim <jungtaek.kim@pitt.edu>
2024-08-02 16:34:34 -0400

    Update read_and_save

--- processing/read_and_save_color_matching_functions.py
+++ processing/read_and_save_color_matching_functions.py
@@ -1,10 +1,10 @@
 import csv
 
 
-if __name__ == '__main__':
+def read_and_save(str_file, file_python, str_variable):
     cmfs = []
 
-    with open('ciexyz31_1.csv') as file_csv:
+    with open(str_file) as file_csv:
         reader = csv.reader(file_csv, delimiter=',')
 
         for row in reader:
@@ -13,13 +13,19 @@ if __name__ == '__main__':
             cmfs.append([row[0], row[1], row[2], row[3]])
 
     assert len(cmfs) == 471
+    file_python.write(f'{str_variable} = [\n')
 
-    with open('../jaxcolors/color_matching_functions.py', mode='w') as file_python:
-        file_python.write('cmfs = [\n')
+    for row in cmfs:
+        file_python.write('    [')
+        file_python.write(', '.join(row))
+        file_python.write('],\n')
 
-        for row in cmfs:
-            file_python.write('    [')
-            file_python.write(', '.join(row))
-            file_python.write('],\n')
+    file_python.write(']')
 
-        file_python.write(']')
+
+if __name__ == '__main__':
+    file_python = open('../jaxcolors/color_matching_functions.py', mode='w')
+
+    read_and_save('ciexyz31_1.csv', file_python, 'cmfs')
+
+    file_python.close()
